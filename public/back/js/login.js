4dbc519cc16745ac3392b0f0dff556edccc92f74
584300905@qq.com <wtz>
2018-06-25 19:21:48 +0800

    登入功能初步校验

---
+++ public/back/js/login.js
@@ -0,0 +1,46 @@
+/**
+ * Created by tz on 2018/6/25.
+ */
+
+
+//1 进行表单校验配置
+//校验要求：用户名不能为空，长度为2-6位 密码不能为空 长度6-12位
+
+$(function(){
+  //表单校验初始化
+  $('#form').bootstrapValidator({
+    //指定校验字段
+    fields: {
+      username:{
+        //配置校验规则
+        validators: {
+          //配置非空校验
+          notEmpty: {
+            message: "用户名不能为空"
+          },
+          //配置长度校验
+          stringLength: {
+            min:2,
+            max:6,
+            message: "用户名长度必须在2-6位"
+          }
+        }
+      },
+      password: {
+        //配置校验规则
+        validators: {
+          //配置非空校验
+          notEmpty: {
+            message: "密码不能为空"
+          },
+          //配置长度校验
+          stringLength: {
+            min:6,
+            max:12,
+            message: "用户名长度必须在6-12位"
+          }
+        }
+      }
+    }
+  });
+})
